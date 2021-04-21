37cceeeea081f9d1f61c66b8c1f5eb35f566d9ac
Munkhjargal <munkhjargalbatdavaa@gmail.com>
2021-04-21 21:47:35 +0800

    Orlogo zarlagiig delgetsen deer gargana

--- app.js
+++ app.js
@@ -19,6 +19,31 @@ var uiController = (function(){
         
         getDOMstrings: function(){
             return DOMstrings;
+        },
+
+        addListItem: function(item, type){
+            // Orlogo zarlagiin elementiig aguulsan HTML iig beltgene.
+            var html, list;
+            
+            if(type === 'inc'){
+                list = ".income__list";
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            else
+            {
+                list = ".expenses__list";
+                html = ' <div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+            // Ter HTML dotroo orlogo zarlagiin utguudiig REPLACE ashiglaj uurchulj ugnu
+
+            html = html.replace("%id%", item.id);
+            html = html.replace("$$DESCRIPTION$$", item.description);
+            html = html.replace("$$VALUE$$", item.value);
+
+            // Beltgesen HTML ee DOM ruu hiij ugnu.
+            document.querySelector(list).insertAdjacentHTML("beforeend", html);
+
         }
     };
 })();
@@ -71,11 +96,11 @@ var financeController = (function(){
 
             if(data.items[type].length === 0 ) id = 1;
             else{
-                id = data.items[type][data.items[type].length - 1].id + 1;
+                id = data.items[type][data.items[type].length - 1].id + 1;   // Umnuh elementih ni id g avad 1iig nemsneer id g n olj bna.
             }
             
 
-            
+
             if(type === "inc"){
                 item = new Income(id, desc, val)
             }
@@ -86,6 +111,8 @@ var financeController = (function(){
             }
 
             data.items[type].push(item);
+
+            return item;
         }
     };
        
@@ -106,10 +133,10 @@ var appController = (function(uiController, financeController)
         var input = uiController.getInput();
 
         // 2. Olj avsan ugugdluudee sanhuugiin controllert damjuulj tend hadgalna. 
-        financeController.addItem(input.type, input.description, input.value);
+        var item = financeController.addItem(input.type, input.description, input.value);
 
         // 3. Olj avsan ugugdluudiig web deeree tohiroh hesegt n gargana.
-
+        uiController.addListItem(item, input.type);
 
         // 4. Tusuviig tootsoolno.
 
